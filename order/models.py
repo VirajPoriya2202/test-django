95d087fa6ac98990389efa31c485c08162eacf1f
VirajPoriya <virajp@hyperlinkinfosystem.net.in>
2024-04-11 17:33:12 +0530

    test

--- order/models.py
+++ order/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class tes(models.Model):
     test1 = models.CharField(max_length=30)
     test2 = models.CharField(max_length=30,null=True,blank=True)
+    test4 = models.CharField(max_length=30,null=True,blank=True)
+    test3 = models.CharField(max_length=30,null=True,blank=True)
